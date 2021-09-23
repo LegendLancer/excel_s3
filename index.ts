d5990b971cff64368088c81005ea1d5d2c89ba09
LegendLancer <legendlancer0314@gmail.com>
2021-09-23 00:12:49 -0700

    Excel2JSON-class-create

--- index.ts
+++ index.ts
@@ -1,95 +1,30 @@
-const http = require('http');
-const node_xj = require("xls-to-json");
-const node_jx = require('excel4node');
-const port = process.env.PORT || 3000;
-
-const exampleAPI = (ids: any) => {
-    let populationData: any = {};
-    for (let key in ids) {
-        populationData[ids[key]] = "blabla";
-    }
-    return populationData;
-}
-let mainData: [] = [], //main data from excel 
-    popData: any = {};  //population data
-
-const xls2json = async () => await node_xj(
+/*
+** fetch total result style **
     {
-        input: "example.xls", // input xls
-        output: "output.json", // output json
-        sheet: "Sheet1", // specific sheetname
-        rowsToSkip: 0, // number of rows to skip at the top of the sheet; defaults to 0
-        allowEmptyKey: false, // avoids empty keys in the output, example: {"": "something"}; default: true
-    },
-    function (err: any, result: any) {
-        if (err) {
-        } else {
-            mainData = result;
-            let ids: any= {}, index:number = 0;
-            for (let obj of result) {
-                ids[index++] = obj['Id'];
-            }
-            // call your API here
-            const populationData = exampleAPI(ids);
-            // set result
-            popData = populationData;
-        }
-    }
-);
-
-const json2xls = async (main: any, population: any) => {
-    console.log(main, population);
-    const result = [];
-    for (let obj of main) {
-        obj['Population'] = population[obj['Id']];
-        result.push(obj);
-    }
-    console.log(result);
-    const wb = new node_jx.Workbook();
-    const ws = wb.addWorksheet('Sheet1');
-
-    const headingColumnNames = [
-        "Id",
-        "Country",
-        "City",
-        "ZipCode",
-        "Population",
+    Sheet1: [
+        { A: 'Id', B: 'Country', C: 'City', D: 'ZipCode' },
+        { A: 123, B: 'US', C: 'Washington', D: 123123 },
+        { A: 333, B: 'UK', C: 'London', D: 123123 },
+        { A: 1112, B: 'Canada', C: 'Ottawa', D: 123123 },
+        { A: 3312, B: 'Russia', C: 'Moscow', D: 123123 }
     ]
-    //Write Column Title in Excel file
-    let headingColumnIndex = 1;
-    headingColumnNames.forEach(heading => {
-        ws.cell(1, headingColumnIndex++)
-            .string(heading)
-    });
-
-
-    //Write Data in Excel file
-    let rowIndex = 2;
-    result.forEach(record => {
-        let columnIndex = 1;
-        Object.keys(record).forEach(columnName => {
-            ws.cell(rowIndex, columnIndex++)
-                .string(record[columnName])
-        });
-        rowIndex++;
-    });
-    wb.write('output.xlsx');
-}
-
-
-
-const server = http.createServer((req: any, res: any) => {
-    console.log(popData);
+    }
     
-    // process goes here
-    xls2json();
-    json2xls(mainData, popData);
+** fetch one column result style: **
+    { '1': 123, '2': 333, '3': 1112, '4': 3312 }
+*/
 
-    res.statusCode = 200;
-    const msg = JSON.stringify("Hello world!");
-    res.end(msg);
-});
+// module import
+const http = require('http');
+const fs = require('fs');
+import Excel2JSON from './Excel2Json';
+const port = process.env.PORT || 3001;
 
-server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}/`);
-});
+// example usage of Excel2JSON class
+let converter = new Excel2JSON();
+converter.setFileName('./example.xls');
+const result = converter.convert();
+console.log(result);
+
+// fetch one column
+console.log(converter.fetchOneColumn('A'));
